4dee1e354b13b82b0ee581b5385a2975ef2f0586
José Sánchez Martínez <118803501+jimmysm1995@users.noreply.github.com>
2025-01-24 12:39:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,18 +9,22 @@ tabs = st.tabs(["Dashboard 1", "Dashboard 2"])
 # Contenido de la pestaña 1
 with tabs[0]:
     st.header("Dashboard 1")
-    st.components.v1.iframe(
-        "https://vinok-metabase.duckdns.org/public/dashboard/e46175a0-4cdf-4cf9-86fa-2b2772ee9285",
-        width=1200,
-        height=800,
-        scrolling=True,
-    )
+    # Incluir el iframe de forma segura en Streamlit
+    iframe_html = """
+    <iframe src="http://9.223.32.5:3000/public/dashboard/9672fa9e-43b9-46ce-baa6-62ab6f1bd0f9" 
+            frameborder="0" 
+            width="800" 
+            height="600" 
+            allowtransparency></iframe>
+    """
+
+st.markdown(iframe_html, unsafe_allow_html=True)
 
 # Contenido de la pestaña 2
 with tabs[1]:
     st.header("Dashboard 2")
     st.components.v1.iframe(
-        "http://9vinok-metabase.duckdns.org/public/dashboard/61e8a95b-8a58-4c1d-9ce6-d788eeed96c3",  # Cambia el URL del segundo dashboard
+        "http://9.223.32.5:3000/public/dashboard/61e8a95b-8a58-4c1d-9ce6-d788eeed96c3",  # Cambia el URL del segundo dashboard
         width=1200,
         height=800,
         scrolling=True,
