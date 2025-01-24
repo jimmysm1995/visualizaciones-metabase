9c6a5a81a6f5c8a491ae09def44b6b3b018bc0ae
José Sánchez Martínez <118803501+jimmysm1995@users.noreply.github.com>
2025-01-24 10:35:23 +0100

    upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+# Configuración de la página
+st.set_page_config(page_title="Dashboards", layout="wide")
+
+# Crear las pestañas
+tabs = st.tabs(["Dashboard 1", "Dashboard 2"])
+
+# Contenido de la pestaña 1
+with tabs[0]:
+    st.header("Dashboard 1")
+    st.components.v1.iframe(
+        "http://9.223.32.5:3000/public/dashboard/e46175a0-4cdf-4cf9-86fa-2b2772ee9285",
+        width=1200,
+        height=800,
+        scrolling=True,
+    )
+
+# Contenido de la pestaña 2
+with tabs[1]:
+    st.header("Dashboard 2")
+    st.components.v1.iframe(
+        "http://9.223.32.5:3000/public/dashboard/61e8a95b-8a58-4c1d-9ce6-d788eeed96c3",  # Cambia el URL del segundo dashboard
+        width=1200,
+        height=800,
+        scrolling=True,
+    )
+
