7134334d7f8f09287ce3f5aadc25806e1d9ed034
José Sánchez Martínez <118803501+jimmysm1995@users.noreply.github.com>
2025-01-24 13:10:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ tabs = st.tabs(["Dashboard 1", "Dashboard 2"])
 with tabs[0]:
     st.header("Dashboard 2")
     st.components.v1.iframe(
-        "https://9.223.32.5:3000/public/dashboard/9672fa9e-43b9-46ce-baa6-62ab6f1bd0f9",  # Cambia el URL del segundo dashboard
+        "https://vinok-metabase.duckdns.org/public/dashboard/9672fa9e-43b9-46ce-baa6-62ab6f1bd0f9",  # Cambia el URL del segundo dashboard
         width=1200,
         height=800,
         scrolling=True,
@@ -20,7 +20,7 @@ with tabs[0]:
 with tabs[1]:
     st.header("Dashboard 2")
     st.components.v1.iframe(
-        "https://9.223.32.5:3000/public/dashboard/61e8a95b-8a58-4c1d-9ce6-d788eeed96c3",  # Cambia el URL del segundo dashboard
+        "https://vinok-metabase.duckdns.org/public/dashboard/61e8a95b-8a58-4c1d-9ce6-d788eeed96c3",  # Cambia el URL del segundo dashboard
         width=1200,
         height=800,
         scrolling=True,
